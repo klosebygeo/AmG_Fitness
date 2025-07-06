c621115d25bea0efc167c7d59dedf17de65be9ea
klosebygeo <georgian.ivan95@gmail.com>
2025-07-06 18:45:55 +0300

    "Add render.yaml to fix deployment2"

--- AmGFitness/settings.py
+++ AmGFitness/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'AmGFitness.urls'
@@ -125,7 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 import os
 
 STATICFILES_DIRS = [
