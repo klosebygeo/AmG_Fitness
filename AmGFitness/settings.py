3697c9b253e3b417ec937d81a9710e3ee173184b
klosebygeo <georgian.ivan95@gmail.com>
2025-07-06 22:04:10 +0300

    test2

--- AmGFitness/settings.py
+++ AmGFitness/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     'home',
     'gallery'
 ]
---
+----
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
